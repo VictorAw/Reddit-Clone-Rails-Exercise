5e1f28cc2a3a7f8d1ae3bffc91c5e2db029482d6
App Academy Student <student@appacademy.io>
2016-10-07 18:59:36 -0700

    Add new notes. Seems like the issue was the browser cache. Clearing browser cache appears to have fixed the problem.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,7 +6,6 @@ class CommentsController < ApplicationController
   def create
     comment = Comment.new(comment_params)
     comment.author_id = current_user.id
-    fail
     comment.save
 
     redirect_to :back
